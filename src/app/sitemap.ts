34c842e8ae483f6346ab64de07ef71788606c676
ytakehir <takegryffindor@gmail.com>
2026-01-16 14:58:17 +0900

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = ''
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? ''
 
   return [
     {
